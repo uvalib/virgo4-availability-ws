63ea6e2ab43d46dba2199f667e6823c0ccc9c63f
Dave Goldstein <dpg3k@virginia.edu>
2020-09-08 10:49:12 -0400

    Correct startup script

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,6 +1,6 @@
 # run application
 
-./bin/v4availability
+./bin/v4availability -ils $ILS_SERVICE -solr $V4_SOLR_URL -core $V4_SOLR_CORE -illiad $V4_ILLIAD_API -illiadkey $V4_ILLIAD_KEY -hsilliad $V4_HSL_ILLIAD_URL -jwtkey $V4_JWT_KEY
 
 #
 # end of file
