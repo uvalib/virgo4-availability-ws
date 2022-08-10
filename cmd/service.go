798de565bf7d639437164bb9a942a25466fcfac0
Nestor Walker <naw4t@virginia.edu>
2022-08-10 16:51:39 -0400

    30 sec timeout

--- cmd/service.go
+++ cmd/service.go
@@ -80,7 +80,7 @@ func intializeService(version string, cfg *ServiceConfig) (*ServiceContext, erro
 	}
 	ctx.SlowHTTPClient = &http.Client{
 		Transport: defaultTransport,
-		Timeout:   20 * time.Second,
+		Timeout:   30 * time.Second,
 	}
 	ctx.initMapLookups()
 
